4fdd6de546461112aee18c47a65eaddebd333b9f
juabiDev <Mederos40@outlook.com>
2023-12-14 11:41:32 -0300

    Antes de separar en componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 import './App.css'
-import books from "./mocks/books.json"
+import initialBooks from "./mocks/books.json"
 
 function App() {
-  const hasBooks = books.library.length > 0 
-  const [listBooks, setListBooks] = useState([]) 
+  const [books, setBooks] = useState(initialBooks.library)
+  const [lectureBooks, setLectureBooks] = useState([]) 
 
   const handleClick = (event) => {
-    const findBook = books.library.find(element => element.book.ISBN === event.target.id)
+    const findBook = books.find(element => element.book.ISBN === event.target.id)
     if(!findBook) return;
-    setListBooks([...listBooks,findBook])
+    const newBooks = books.filter(element => element.book.ISBN !== event.target.id)
+    setBooks(newBooks)
+    const newLectureBooks = structuredClone(lectureBooks)
+    setLectureBooks([...newLectureBooks,findBook])
+  }
+
+  const handleDrop = (event) => {
+    const findBook = lectureBooks.find(element => element.book.ISBN === event.target.id)
+    if(!findBook) return;
+    const newlectureBooks = lectureBooks.filter(element => element.book.ISBN !== event.target.id)
+    setBooks(prevState => [...prevState, findBook])
+    setLectureBooks(newlectureBooks)
   }
 
   return (
@@ -19,7 +30,7 @@ function App() {
         <section>
           <h2>Libros Disponibles</h2>
           <ul>
-            {hasBooks && books.library.map((item) => (
+            {books && books.map((item) => (
                 <li key={item.book.ISBN}>
                   <img src={item.book.cover} alt={`Book ${item.book.title}`}/>
                   <h3>{item.book.title}</h3>
@@ -36,9 +47,17 @@ function App() {
         <section>
           <h2>Lista de Lectura</h2>
           <ul>
-              {listBooks && listBooks.map((item, index) => {
-                console.log(item)
-              })}
+              {lectureBooks && lectureBooks.map((item) => (
+                <li key={item.book.ISBN}>
+                  <img src={item.book.cover} alt={`Book ${item.book.title}`}/>
+                  <h3>{item.book.title}</h3>
+                  <p>Year: {item.book.year}</p>
+                  <p>Autor: {item.book.author.name}</p>
+                  <p>Pages: {item.book.pages}</p>
+                  <strong>Genre: {item.book.genre} </strong>
+                  <button id={item.book.ISBN} onClick={handleDrop}>Eliminar</button>
+                </li> 
+              ))}
           </ul>
         </section>
       </main>
